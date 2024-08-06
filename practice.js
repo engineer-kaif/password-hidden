f595d3618e88fd2517c8240491145a498e466721
md kaif <mdkaifwtca@gmail.com>
2024-08-06 17:22:48 +0530

    password hidden code

---
+++ practice.js
@@ -0,0 +1,17 @@
+let eyeicon=document.getElementById('eyeicon')
+let password=document.getElementById('password')
+
+eyeicon.addEventListener('click',function(){
+    if(password.type=="password"){
+        password.type="text"
+        
+    }
+    else{
+        password.type="password"
+        eyeicon.color=pink;
+    }
+})
+    
+
+
+
